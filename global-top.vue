6505d5f393aa9376f6fc0281a5ae7e7a544fa7f8
Michael Soltys <msoltys@gmail.com>
2026-02-10 13:53:07 -0800

    Update slides — 2026-02-10 13:53

---
+++ global-top.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="absolute top-4 right-4 z-10">
+    <img src="/NAML26_generic(light-bg).png" class="w-16 opacity-80" alt="NAML 2026" />
+  </div>
+  <div class="absolute bottom-4 left-4 z-10 text-sm opacity-50">
+    {{ $nav.currentPage }} / {{ $nav.total }}
+  </div>
+</template>
